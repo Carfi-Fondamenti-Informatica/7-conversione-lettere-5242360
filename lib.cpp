c96a62579e6bb10368d89dea07f9cec8b872cfe8
5242360 <100364230+5242360@users.noreply.github.com>
2022-05-06 16:53:28 +0200

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,14 @@
 
+#include "lib.h"
+bool convertilettera( char &c) {
+    if (!((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))) {
+        return false;
+    }
+     if(c>=65&&c<=90){
+         c=c+32;
+         return true;
+     }else{
+         c=c-32;
+         return true;
+     }
+}
